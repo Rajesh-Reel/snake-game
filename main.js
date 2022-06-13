b96353e485f87268f61199ce85d01ce3c5886a64
Rajesh-Reel <rajreel2@hotmail.com>
2022-06-13 09:21:15 +0100

    snake moves

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ class Apple{
 
 let canvas = document.getElementById("canvas");
 
-let snake = new Snake();
+let snake = new Snake(20,20,20);
 
 let apple = new Apple();
 
@@ -82,6 +82,8 @@ function show(){
 }
 
 function update(){
+    canvasContext.clearRect(0,0, canvas.width, canvas.height)
+    snake.move()
 
 }
 
@@ -104,3 +106,21 @@ function createRect(x,y,width, height, color){
     canvasContext.fillStyle = color
     canvasContext.fillRect(x,y,width, height)
 }
+
+window.addEventListener("keydown", (event) => {
+    setTimeout(() => {
+        if(event.keyCode == 37 && snake.rotateX != 1){
+            snake.rotateX = -1
+            snake.rotateY = 0;
+        } else if(event.keyCode == 38 && snake.rotateY != 1){
+            snake.rotateX = 0
+            snake.rotateY = -1;
+        } else if(event.keyCode == 39 && snake.rotateX != -1){
+            snake.rotateX = 1
+            snake.rotateY = 0;
+        } else if(event.keyCode == 40 && snake.rotateY != -1){
+            snake.rotateX = 0
+            snake.rotateY = 1;
+        }
+    }, 1)
+})
